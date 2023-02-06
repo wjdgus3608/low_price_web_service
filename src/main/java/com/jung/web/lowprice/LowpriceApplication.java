31a50009198ead57e77aaa72315fe8794e534155
wjdgus3608 <wjdgus3608@naver.com>
2023-02-07 00:48:26 +0900

    init project

---
+++ src/main/java/com/jung/web/lowprice/LowpriceApplication.java
@@ -0,0 +1,13 @@
+package com.jung.web.lowprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jung/web/lowprice/LowpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jung.web.lowprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

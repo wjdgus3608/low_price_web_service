1a73cf3295179b59bbab5f46d74d5748cdbd0098
wjdgus3608 <wjdgus3608@naver.com>
2023-02-14 01:13:11 +0900

    test module coding

---
+++ module-webpage/src/main/java/com/jung/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.jung.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
+    }
+}

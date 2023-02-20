665426d8ee839ff6dfc65c9682cf32023aa0e4dd
wjdgus3608 <wjdgus3608@naver.com>
2023-02-20 22:51:40 +0900

    Controller 형식 코딩

---
+++ module-api-usage/src/main/java/com/jung/controller/ApiUsageController.java
@@ -0,0 +1,34 @@
+package com.jung.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiUsageController {
+    @GetMapping("/current-usage")
+    public ResponseEntity<?> getCurrentUsage(){
+
+    }
+
+    @GetMapping("/max-usage")
+    public ResponseEntity<?> getMaxUsage(){
+
+    }
+
+    @PostMapping("/api")
+    public ResponseEntity<?> addApi(){
+
+    }
+
+    @PostMapping("/usage")
+    public ResponseEntity<?> increaseUsage(){
+
+    }
+
+    @PostMapping("/usage-zero")
+    public ResponseEntity<?> initUsage(){
+        
+    }
+}

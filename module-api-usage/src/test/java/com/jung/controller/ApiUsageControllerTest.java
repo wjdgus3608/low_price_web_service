0f28beb5d2718703a6d2a14cfe3db7361ea90120
wjdgus3608 <wjdgus3608@naver.com>
2023-02-25 13:42:52 +0900

    통합테스트로 수정하기

--- module-api-usage/src/test/java/com/jung/controller/ApiUsageControllerTest.java
+++ module-api-usage/src/test/java/com/jung/controller/ApiUsageControllerTest.java
@@ -10,32 +10,37 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import org.springframework.transaction.annotation.Transactional;
 
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@WebMvcTest(ApiUsageController.class)
+//@WebMvcTest(ApiUsageController.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class ApiUsageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+//
+//    @MockBean
 
-    @MockBean
-    private ApiUsageService apiUsageService;
-
+//    private ApiUsageService apiUsageService;
     private ResponseEntity<?> entity;
 
     ApiUsageControllerTest(){
@@ -47,14 +52,21 @@ class ApiUsageControllerTest {
     }
 
     @BeforeAll
-    void addTestApi(){
-        ApiUsageDTO apiUsageDTO = ApiUsageDTO.builder()
-                .apiType(ApiType.SHOPPING_API)
-                .currentUsage(0)
-                .maxUsage(25000)
-                .build();
-        apiUsageService.addApi(apiUsageDTO);
+    void addTestApi() throws Exception {
+//        ApiUsageDTO apiUsageDTO = ApiUsageDTO.builder()
+//                .apiType(ApiType.SHOPPING_API)
+//                .currentUsage(0)
+//                .maxUsage(25000)
+//                .build();
+//        apiUsageService.addApi(apiUsageDTO);
 
+        String strEntity = objectMapper.writeValueAsString(entity.getBody());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api")
+                    .content(strEntity)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
 
     }
 
@@ -62,7 +74,7 @@ class ApiUsageControllerTest {
     @DisplayName("api 현재 사용량 조회")
     void getCurrentUsage() throws Exception {
         //given
-        doReturn(entity).when(apiUsageService).getUsage();
+//        doReturn(entity).when(apiUsageService).getUsage();
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/current-usage"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -70,7 +82,7 @@ class ApiUsageControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("currentUsage").value(equalTo(0)));
     }
 
-    @Test
+/*    @Test
     @DisplayName("api 최대 사용량 조회")
     void getMaxUsage() throws Exception {
         //given
@@ -109,7 +121,7 @@ class ApiUsageControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("currentUsage").value(equalTo(0)))
                 .andDo(print());
-    }
+    }*/
 
     @Test
     void initUsage() {

972cb3d823e521f87d829b03279c9381115c86bf
wjdgus3608 <wjdgus3608@naver.com>
2023-03-05 21:43:31 +0900

    UserService, signUp 틀 구현

---
+++ module-user/src/main/java/com/jung/service/UserService.java
@@ -0,0 +1,22 @@
+package com.jung.service;
+
+import com.jung.domain.user.UserDTO;
+import com.jung.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+    private final UserRepository userRepository;
+
+    public ResponseEntity<?> signUp(UserDTO userDTO){
+
+        return ResponseEntity.ok().build();
+    }
+}

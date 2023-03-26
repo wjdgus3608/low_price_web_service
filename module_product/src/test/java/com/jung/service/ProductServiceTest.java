b074a2bdd2d9fa130665f65d788f804743cd8d57
wjdgus3608 <wjdgus3608@naver.com>
2023-03-26 18:03:30 +0900

    searchProduct 테스트 구현

--- module_product/src/test/java/com/jung/service/ProductServiceTest.java
+++ module_product/src/test/java/com/jung/service/ProductServiceTest.java
@@ -1,12 +1,18 @@
 package com.jung.service;
 
+import com.jung.domain.product.Product;
 import com.jung.domain.product.SearchInfo;
 import com.jung.domain.product.SortType;
+import com.jung.domain.user.User;
+import com.jung.domain.user.UserDTO;
+import com.jung.domain.user.UserType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +36,12 @@ class ProductServiceTest {
     }
     @Test
     void searchProduct() {
-        
+        //given
+
+        //when
+        List<Product> products = searchInfo.searchProduct(searchInfo);
+        //than
+        assertEquals(100,products.size());
+        assertEquals(true,products.get(0).getProductName().contains("삼겹살"));
     }
 }

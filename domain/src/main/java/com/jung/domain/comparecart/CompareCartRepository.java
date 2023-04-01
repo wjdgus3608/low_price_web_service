cec336a439ac853986fd78f2647c24f6cbb37926
wjdgus3608 <wjdgus3608@naver.com>
2023-04-01 09:53:39 +0900

    CompareCartRepository 생성

---
+++ domain/src/main/java/com/jung/domain/comparecart/CompareCartRepository.java
@@ -0,0 +1,6 @@
+package com.jung.domain.comparecart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompareCartRepository extends JpaRepository<CompareCart,Long> {
+}

36909d7d59a6c8f1bbc470722628b6f741f1975b
wjdgus3608 <wjdgus3608@naver.com>
2023-04-03 00:23:18 +0900

    comparecart 모듈 생성

---
+++ module-comparecart/src/main/java/com/jung/CompareCartApplication.java
@@ -0,0 +1,13 @@
+package com.jung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareCartApplication {
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(CompareCartApplication.class, args);
+    }
+}

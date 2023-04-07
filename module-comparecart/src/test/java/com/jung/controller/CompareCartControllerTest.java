b6e5e5c972858710f2af51e6725ab301b37282fe
wjdgus3608 <wjdgus3608@naver.com>
2023-04-07 22:26:14 +0900

    비교카트 테스트 초기설정 코드 작성

--- module-comparecart/src/test/java/com/jung/controller/CompareCartControllerTest.java
+++ module-comparecart/src/test/java/com/jung/controller/CompareCartControllerTest.java
@@ -1,15 +1,21 @@
 package com.jung.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jung.domain.comparecart.CompareCart;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
@@ -20,6 +26,23 @@ class CompareCartControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+
+    private CompareCart firstCompareCart;
+
+    @BeforeAll
+    @DisplayName("비교카트 테스트 초기 설정")
+    void initCart() throws Exception {
+        firstCompareCart = CompareCart.builder()
+                .ownerId("user1")
+                .build();
+        String strEntity = objectMapper.writeValueAsString(firstCompareCart);
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/compare-cart")
+                .content(strEntity)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
 
     @Test
     @DisplayName("비교카트 생성")

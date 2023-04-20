822fd29024527ae852212a2362ea9edcec2ab875
wjdgus3608 <wjdgus3608@naver.com>
2023-04-21 00:15:24 +0900

    FilterKeywordService 템플릿 구현

--- module-filterkeyword/src/main/java/com/jung/service/FilterKeywordService.java
+++ module-filterkeyword/src/main/java/com/jung/service/FilterKeywordService.java
@@ -1,6 +1,8 @@
 package com.jung.service;
 
+import com.jung.domain.filterkeyword.FilterKeywordRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,5 +10,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class FilterKeywordService {
-    private final
+    private final FilterKeywordRepository filterKeywordRepository;
+
+    public ResponseEntity<?> generateFilterKeyword(){
+
+    }
+
+    public ResponseEntity<?> deleteFilterKeyword(){
+
+    }
+
+    public ResponseEntity<?> addExcludeKeywordToFilterKeyword(){
+
+    }
+
+    public ResponseEntity<?> removeExcludeKeywordFromFilterKeyword(){
+
+    }
 }

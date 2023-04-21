9389117173b3483c1e7b0f3fe9a8cf860e6efd42
wjdgus3608 <wjdgus3608@naver.com>
2023-04-21 23:47:35 +0900

    필터키워드 검색 테스트 구현

--- module-filterkeyword/src/test/java/com/jung/service/FilterKeywordServiceTest.java
+++ module-filterkeyword/src/test/java/com/jung/service/FilterKeywordServiceTest.java
@@ -1,11 +1,15 @@
 package com.jung.service;
 
+import com.jung.domain.filterkeyword.FilterKeyword;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +24,19 @@ class FilterKeywordServiceTest {
     void setFilterKeyword(){
 //        filterKeywordService.generateFilterKeyword("user1");
     }
+
+    @Test
+    @DisplayName("필터키워드 검색")
+    void searchFilterKeyword(){
+        //given
+        //when
+        Optional<FilterKeyword> findKeyword = filterKeywordService.searchFilterKeyword(searchInfo);
+        //then
+        assertTrue(findKeyword.isPresent());
+        assertEquals("user1",findKeyword.get().getOwnerId());
+        assertEquals("keyword1",findKeyword.get().getSearchKeyword());
+    }
+
     @Test
     void generateFilterKeyword() {
     }

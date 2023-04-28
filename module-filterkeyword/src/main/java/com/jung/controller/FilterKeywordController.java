d181c9a78ecbfbee006bd8cdbfc8998df57a9f80
wjdgus3608 <wjdgus3608@naver.com>
2023-04-28 20:19:45 +0900

    FilterKeywordController 템플릿 생성

---
+++ module-filterkeyword/src/main/java/com/jung/controller/FilterKeywordController.java
@@ -0,0 +1,31 @@
+package com.jung.controller;
+
+import com.jung.service.FilterKeywordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class FilterKeywordController {
+    private final FilterKeywordService filterKeywordService;
+/*
+    public ResponseEntity<?> searchKeywordByInfo(){
+
+    }
+    public ResponseEntity<?> generateFilterKeyword(){
+
+    }
+    public ResponseEntity<?> deleteFilterKeyword(){
+
+    }
+    public ResponseEntity<?> searchExcludeKeyword(){
+
+    }
+    public ResponseEntity<?> addExcludeKeywordToFilterKeyword(){
+
+    }
+    public ResponseEntity<?> removeExcludeKeywordFromFilterKeyword(){
+
+    }*/
+}

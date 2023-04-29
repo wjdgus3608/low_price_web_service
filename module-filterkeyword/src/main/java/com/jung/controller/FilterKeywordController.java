59694776f494c9fb3e405c1adc45c42330909f50
wjdgus3608 <wjdgus3608@naver.com>
2023-04-29 18:29:04 +0900

    필터키워드 생성 함수 구현

--- module-filterkeyword/src/main/java/com/jung/controller/FilterKeywordController.java
+++ module-filterkeyword/src/main/java/com/jung/controller/FilterKeywordController.java
@@ -1,8 +1,11 @@
 package com.jung.controller;
 
+import com.jung.domain.filterkeyword.FilterKeywordDTO;
 import com.jung.service.FilterKeywordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilterKeywordController {
     private final FilterKeywordService filterKeywordService;
 
+    @PostMapping("/filterkeyword")
+    public ResponseEntity<?> generateFilterKeyword(@RequestBody FilterKeywordDTO keywordDTO){
+        return filterKeywordService.generateFilterKeyword(keywordDTO);
+    }
+
     /*public ResponseEntity<?> searchKeywordByInfo(){
 
     }
-    public ResponseEntity<?> generateFilterKeyword(){
 
-    }
     public ResponseEntity<?> deleteFilterKeyword(){
 
     }

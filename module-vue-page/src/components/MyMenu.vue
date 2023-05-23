40fc20d87ea34b21b87492b0ee56f6c92d2ae9e5
wjdgus3608 <wjdgus3608@naver.com>
2023-05-23 18:11:25 +0900

    메뉴목록 수정

--- module-vue-page/src/components/MyMenu.vue
+++ module-vue-page/src/components/MyMenu.vue
@@ -1,15 +1,26 @@
 <template>
     <div id="myMenu">
-       
+        <div class="row">
+            <div class="col-4">
+                <div class="list-group" id="list-tab" role="tablist">
+                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list"
+                        href="#list-home" role="tab" aria-controls="list-home">키워드 관리</a>
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list"
+                        href="#list-profile" role="tab" aria-controls="list-profile">승인 관리</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
 <style scoped>
-
+.row {
+    width: 700px;
+}
 </style>
--- module-vue-page/src/components/MyPage.vue
+++ module-vue-page/src/components/MyPage.vue
@@ -1,23 +1,37 @@
 <template>
     <div id="myPage">
-       
+       <NavBar/>
+        <div id="myPageContainer">
+            <MyMenu/>
+        </div>
     </div>
 </template>
 
 <script>
-// import MyMenu from './MyMenu.vue';
+import MyMenu from './MyMenu.vue';
 // import KeywordsSubPage from './KeywordsSubPage.vue';
 // import AdminSubPage from './AdminSubPage.vue';
+import NavBar from './NavBar.vue'
 
 export default {
     components:{
-        // MyMenu,
+        MyMenu,
         // KeywordsSubPage,
         // AdminSubPage
+        NavBar
     }
 }
 </script>
 
 <style scoped>
+#myPageContainer {
+    margin: 200px 50px;
+    width: 100%;
+    height: 100%;
+}
 
+#subPageContainer {
+    width: 100%;
+    height: 100%;
+}
 </style>

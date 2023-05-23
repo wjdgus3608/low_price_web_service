8df116fb863b7375802d3f36a8e74a31c317b909
wjdgus3608 <wjdgus3608@naver.com>
2023-05-23 11:19:00 +0900

    회원가입 화면 구현

--- module-vue-page/src/components/SignUp.vue
+++ module-vue-page/src/components/SignUp.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    signup view
+  <div class="container" id="signupMain">
+    <title>회원가입</title>
+        <h3>회원가입</h3>
+        <form id="signupBox">
+            <div class="mb-3">
+                <label for="userId" class="form-label">ID</label>
+                <input type="text" class="form-control" id="userId" v-model="userId" placeholder="ID">
+            </div>
+            <div class="mb-3">
+                <label for="userPw" class="form-label">Password</label>
+                <input type="password" class="form-control" id="userPw" v-model="password" placeholder="Password">
+            </div>
+            <div class="mb-3">
+                <label for="userName" class="form-label">Password</label>
+                <input type="text" class="form-control" id="userName" v-model="userName" placeholder="이름">
+            </div>
+            <button type="submit" class="btn btn-primary" id="signupBtn" @click="submitForm">가입신청</button>
+        </form>
   </div>
 </template>
 
@@ -10,6 +26,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+#signupMain {
+  margin-top: 40px;
+}
+#signupBtn {
+    float: right;
+}
 </style>

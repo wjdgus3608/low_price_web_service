15a8eef66e65f6f58e3dd00d20e1229871c741ab
wjdgus3608 <wjdgus3608@naver.com>
2023-05-29 22:46:36 +0900

    단위 콤보박스 추가

--- module-vue-page/src/components/CompareCart.vue
+++ module-vue-page/src/components/CompareCart.vue
@@ -1,27 +1,40 @@
 <template>
     <div id="compareCart">
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
-        <CartItemCard/>
+        <div id="compareCartItemContainer">
+            <CartItemCard />
+            <CartItemCard />
+            <CartItemCard />
+            <CartItemCard />
+            <CartItemCard />
+            <CartItemCard />
+        </div>
+        <div id="compareCartToolContainer">
+            <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
     </div>
 </template>
 
 <script>
 import CartItemCard from './CartItemCard.vue';
 export default {
-   components:{
-    CartItemCard
-   }
+    components: {
+        CartItemCard
+    }
 }
 </script>
 
 <style scoped>
-#compareCart {
+#compareCartItemContainer {
     overflow: scroll;
-    height: 600px;
+    height: 550px;
 }
 
+#compareCartToolContainer {
+    background-color: green;
+}
 </style>

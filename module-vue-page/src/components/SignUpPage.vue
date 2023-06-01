a4a19821f5f43cf7eaf4486c9794940fc953e883
wjdgus3608 <wjdgus3608@naver.com>
2023-06-01 15:37:05 +0900

    회원가입 폼 체크 기능 및 api 연결

--- module-vue-page/src/components/SignUpPage.vue
+++ module-vue-page/src/components/SignUpPage.vue
@@ -1,40 +1,83 @@
 <template>
-  <div class="container" id="signupMain">
-    <title>회원가입</title>
+    <div class="container" id="signupMain">
+        <title>회원가입</title>
         <h3>회원가입</h3>
-        <form id="signupBox">
+        <form id="signupBox" @submit="callSignUpApi">
             <div class="mb-3">
                 <label for="userId" class="form-label">ID</label>
                 <input type="text" class="form-control" id="userId" v-model="userId" placeholder="ID">
             </div>
             <div class="mb-3">
                 <label for="userPw" class="form-label">Password</label>
-                <input type="password" class="form-control" id="userPw" v-model="password" placeholder="Password">
+                <input type="password" class="form-control" id="userPw" v-model="userPw" placeholder="Password">
             </div>
             <div class="mb-3">
                 <label for="userPwRe" class="form-label">Repeat Password</label>
-                <input type="password" class="form-control" id="userPwRe" v-model="passwordRe" placeholder="Repeat Password">
+                <input type="password" class="form-control" id="userPwRe" v-model="passwordRe"
+                    placeholder="Repeat Password">
+                    <p v-show="passwordRe!='' && userPw!==passwordRe" style="color: red;">비밀번호가 일치하지 않습니다.</p>
             </div>
             <div class="mb-3">
                 <label for="userName" class="form-label">이름</label>
                 <input type="text" class="form-control" id="userName" v-model="userName" placeholder="이름">
             </div>
-            <button type="submit" class="btn btn-primary" id="signupBtn" @click="submitForm">가입신청</button>
+            <button type="submit" class="btn btn-primary" id="signupBtn">가입신청</button>
         </form>
-  </div>
+    </div>
 </template>
 
 <script>
-export default {
+import axios from "axios"
 
+export default {
+    methods: {
+        checkForm(){
+            if(!this.userId || !this.userPw || !this.userName || !this.passwordRe){
+                alert("필수값이 입력되지 않았습니다.");
+                return false;
+            }
+            if(this.userPw!==this.passwordRe){
+                alert("비밀번호가 일치하지 않습니다.");
+                return false;
+            }
+            return true;
+        },
+        callSignUpApi(event) {
+            event.preventDefault();
+            if(!this.checkForm()){
+                return;
+            }
+            
+            axios.post('http://localhost:6060/user', {
+                userId: this.userId,
+                userPw: this.userPw,
+                userName: this.userName
+            })
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    },
+    data(){
+        return{
+            userId: '',
+            userPw: '',
+            userName: '',
+            passwordRe: ''
+        }
+    }
 }
 </script>
 
 <style scoped>
 #signupMain {
-  margin-top: 40px;
-  width: 800px;
+    margin-top: 40px;
+    width: 800px;
 }
+
 #signupBtn {
     float: right;
 }

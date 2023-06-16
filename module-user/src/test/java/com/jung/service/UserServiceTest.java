d625fa8ea698a7c7fc04014a3f1b961eb1871aac
wjdgus3608 <wjdgus3608@naver.com>
2023-06-16 14:34:07 +0900

    user 테스트 db 분리

--- module-user/src/test/java/com/jung/service/UserServiceTest.java
+++ module-user/src/test/java/com/jung/service/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 import java.util.Optional;
